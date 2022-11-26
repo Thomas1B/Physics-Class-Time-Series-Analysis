f4fae78132b8ca9e84a3688d165a545838f39e35
Thomas <thomasbourgeois2@hotmail.com>
2022-11-25 16:04:11 -0800

    updating mastering function

--- main_functions.py
+++ main_functions.py
@@ -1,6 +1,6 @@
 # Modules for master functions.
 
-def showModules(module):
+def showModules(moduleName):
     '''
     Function to print names of modules
 
@@ -8,5 +8,5 @@ def showModules(module):
         modules (str): name of module
     '''
     print("List of available functions:")
-    for i, func in enumerate(module.function_list):
-        print(" {}: {}".format(i+1, func.__name__))
+    for i, func in enumerate(moduleName.function_list):
+        print("   {}: {}".format(i+1, func.__name__))
