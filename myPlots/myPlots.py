c5db95db01632ff9bd9f8cf6587b4a9e60158e68
Thomas <thomasbourgeois2@hotmail.com>
2022-12-14 14:35:35 -0800

    made marker sizes bigger

--- myPlots/myPlots.py
+++ myPlots/myPlots.py
@@ -56,7 +56,14 @@ def plotLocalHeatMap(stuff,
 
 
     cbar = plt.colorbar(c, format=barFormat, pad=0.02) # adding a color bar.
-    plt.scatter(stations.long, stations.lati, color='white', edgecolor='r', label='Station')  # plotting station locations.
+    # plotting station locations.
+    plt.scatter(stations.long,
+                stations.lati, 
+                color='white',
+                edgecolor='r', 
+                label='Station',
+                s=50,
+                zorder=10)  
 
 
     # limits for plot
@@ -77,11 +84,14 @@ def plotLocalHeatMap(stuff,
 
 
 
-def plotGlobalHeatMap(data, 
-                 title='', 
+def plotGlobalHeatMap(stuff, 
+                 title='',
+                 barFormat='%.1f',
+                 style='pcolor',
+                 fontsize=12,
                  axisLimits=False):
 
-    xi, yi, temps, coastline, locations = data
+    xi, yi, temps, coastline, locations = stuff
 
     ax = plt.axes()
     ax.set_facecolor('steelblue') # coloring ocean.
@@ -89,13 +99,23 @@ def plotGlobalHeatMap(data,
     plt.plot(coastline.long, coastline.lati, color="k", linewidth=0.5) # plotting land
     plt.fill(coastline.long, coastline.lati, zorder=0, color="olivedrab") # coloring land
 
-
+    # Coloring heat map
     cmap = plt.get_cmap('jet')
-    c = plt.contourf(xi, yi, temps, 15, cmap=cmap)
-    plt.colorbar(c, format='%.2f') # adding a color bar.
+    c = ''
+    if  style == 'contourf':
+        c = plt.contourf(xi, yi, zi, 16, cmap=cmap)
+        plt.contour(xi, yi, zi, 15, colors='k', linewidths=0.5, alpha=0.5)
+
+    else:
+        c = plt.pcolor(xi, yi, zi, cmap=cmap)
+        plt.rcParams['pcolor.shading'] = 'auto'
 
     fontsize=12
-    plt.scatter(locations.long, locations.lati, color='white', edgecolor='r', label="Station")  # plotting station locations.
+    plt.scatter(locations.long, 
+                locations.lati,
+                color='white',
+                edgecolor='r',
+                label="Station")  # plotting station locations.
     plt.title(title, fontsize=fontsize+4, pad=10)
     plt.xlabel("Longtitude [$^{\circ}$ $W$]", fontsize=fontsize, labelpad=10)
     plt.ylabel('Latitude [$^{\circ}$ $N$]', fontsize=fontsize, labelpad=10)
@@ -106,6 +126,14 @@ def plotGlobalHeatMap(data,
         mins = station_info.min()[1:3] - 0.05
         plt.xlim(mins.long)
         plt.ylim(mins.lati, maxs.lati)
+
+    plt.title(title, fontsize=fontsize+4, pad=15)
+    plt.xlabel("Longtitude [$^{\circ}$ $W$]", fontsize=fontsize, labelpad=10)
+    plt.ylabel('Latitude [$^{\circ}$ $N$]', fontsize=fontsize, labelpad=10)
+    plt.legend(fontsize=fontsize)
+    
+    plt.tick_params(labelsize=fontsize-2)
+    cbar.ax.tick_params(labelsize=fontsize-2)
 
 # List of functions. 
 function_list = [plotLocalHeatMap, plotGlobalHeatMap]
